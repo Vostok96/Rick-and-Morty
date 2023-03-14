01824eeef229c3c1d208bb2da7ef332526861657
Denis R <denis_15_abril@hotmail.com>
2023-03-14 18:54:41 -0500

    terminado

--- src/components/ResidentInfo.jsx
+++ src/components/ResidentInfo.jsx
@@ -21,7 +21,7 @@ const ResidentInfo = ({residentUrl} ) => {
             <div className="card">
                 <img className="imgResident" src={resident.image} alt="" />
                 <h1>{resident.name}</h1>
-                <h2><span style={{ fontSize: '70px', color: resident.status === 'Alive' ? 'red' : 'grey' }} >.</span> <span > {resident.status}</span> </h2>
+                <h2><span style={{ fontSize: '70px', color: resident.status === 'Alive' ? 'red' : 'grey' }} >.</span> <span> {resident.status}</span> </h2>
                 <h3> <span className="word">Raza:</span><span className="concept">{resident.species}</span></h3>
                 <h3> <span className="word">Lugar de origen: </span><span className="concept">{resident.origin?.name}</span></h3>
                 <h3> <span className="word">Apariciones: </span><span className="concept">{resident.episode?.length}</span></h3>
